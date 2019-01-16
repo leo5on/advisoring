8af7a4cc154b3fd412de8c729828ab22ffbcff3f
Leandro Nozela <nozela.leo@gmail.com>
2019-01-16 14:08:40 -0300

    Add files via upload
    Full functions of the Futaba joystick using the xbox controller.

--- warthog_teleop.py
+++ warthog_teleop.py
@@ -25,20 +25,24 @@ def tj_callback(data):
     # start publisher of cmd_vel to control the simulation in Gazebo
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
-    # Create Twist message & add linear x and angular z from left joystick
-    twist = Twist()
-    twist.linear.x = data.axes[1]
-    twist.angular.z = data.axes[0]
+    # Create Twist message & add linear x from left analog and angular z from right analog
+    if data.buttons[1] == 0:                # velocity button check (B button)
+        twist = Twist()
+        twist.linear.x = data.axes[1]       # vertical moviment of left analog
+        twist.angular.z = data.axes[3]      # horizontal moviment of right analog
+    else: 
+        twist = Twist()
+        twist.linear.x = 2 * data.axes[1]       
+        twist.angular.z = 2 * data.axes[3]
 
     # record values to log file and screen
     rospy.loginfo("twist.linear: %f ; angular %f", twist.linear.x, twist.angular.z)
 
-    # process joystick buttons
-    #if data.buttons[0] == 1:        # green button on xbox controller
-        #linear.x = 1.0
-
-    # publish cmd_vel move command to the /cmd_vel topic
-    pub.publish(twist)
+    # process joystick LB button as a enable/disable button
+    if data.buttons[4] == 1:        # LB button on xbox controller
+        
+        # publish cmd_vel move command to the /cmd_vel topic
+        pub.publish(twist)
 
 if __name__ == '__main__':
     try:
