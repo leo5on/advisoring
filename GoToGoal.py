651e947f369421d086079d8cb0492cc0c823fdad
Leandro Nozela <nozela.leo@gmail.com>
2019-02-05 17:05:58 -0300

    Actual Version

--- GoToGoal.py
+++ GoToGoal.py
@@ -1,100 +1,73 @@
 #!/usr/bin/env python
 import rospy
+import math
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-from math import pow, atan2, sqrt
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Pose
+from tf.transformations import euler_from_quaternion
 
 
-class Warthog:
+class Warthog():
 
-    def __init__(self):
-        # Creates a node with name 'turtlebot_controller' and make sure it is a unique node (using anonymous=True).
+    def __init__(self, odom_topic, cmd_vel_topic, tolerance=0.1):
+        # Creates a node with name 'warthog_controller' and make sure it is a unique node (using anonymous=True).
         rospy.init_node('warthog_controller', anonymous=True)
 
         # Publisher which will publish to the topic '/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.velocity_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-        # A subscriber to the topic '/warthog/pose'. self.update_pose is called when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('/warthog/pose', Pose, self.update_pose)
-        self.pose = Pose()
-        self.rate = rospy.Rate(10)
+        # Subscriber to the Odom data:
+        self._odomSubscriber = rospy.Subscriber("/odometry/filtered", Odometry, self.get_position_odom)
 
-        # A publisher to the '/warthog/pose' topic. This topic will withhold the actual robot pose.
-        self.pose_publisher = rospy.Publisher('/warthog/pose', Pose, queue_size=10)
+        # Internal Variables iniciation
+        self._odomTopic = odom_topic
+        self._cmd_velTopic = cmd_vel_topic
+        self._tolerance = tolerance
+        self._pose = [0, 0, 0]
+        self.goal_pose = [0, 0]
+        self._end = False
 
-        # A subscriber/publisher to a topic where the user will give the goal input
-        self.pose_subscriber = rospy.Subscriber('/goal/input', Pose, self.update_pose)
-        self.pose_publisher = rospy.Publisher('/goal/input', Pose, queue_size=10)
+        # Gets the inputs from the user
+        self.goal_pose[0] = input("Set your x goal: ")
+        self.goal_pose[1] = input("Set your y goal: ")
 
-    def update_pose(self, data):
-        """Callback function which is called when a new message of type Pose is
-        received by the subscriber."""
-        self.pose = data
-        self.pose.x = round(self.pose.x, 4)
-        self.pose.y = round(self.pose.y, 4)
+    # Function that converts the Odometry data to Pose type msg
+    def get_position_odom(self, msg):
+        self._pose[0] = msg.pose.pose.position.x
+        self._pose[1] = msg.pose.pose.position.y
+        rot_q = msg.pose.pose.orientation
+        _, _, self._pose[2] = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
-    def euclidean_distance(self, goal_pose):
-        """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
-                    pow((goal_pose.y - self.pose.y), 2))
-
-    def linear_vel(self, goal_pose, constant=1.5):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * self.euclidean_distance(goal_pose)
-
-    def steering_angle(self, goal_pose):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
-
-    def angular_vel(self, goal_pose, constant=6):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
-
+    # Function to move the robot
     def move2goal(self):
-        """Moves the turtle to the goal."""
-        goal_pose = Pose()
+        if (not self._end):
+            velocityCommand = Twist()
+            velocityCommand.linear.x = 0.5
+            # Calculation of the distance between the two points
+            distance = math.sqrt((self.goal_pose[0] - self._pose[0])**2 + (self.goal_pose[1] - self._pose[1])**2)
+            if(distance <= self._tolerance):
+                self._end = True
+                velocityCommand.angular.z = 0
+                velocityCommand.linear.x = 0
+            else :
+                teta = math.atan2((self.goal_pose[1] - self._pose[1]), (self.goal_pose[0] - self._pose[0]))
+                erroAngle = (teta - self._pose[2])
+                if(erroAngle > 3.1415):
+                    erroAngle -= 2*3.1415
+                elif(erroAngle < -3.1415):
+                    erroAngle += 2*3.1415
+                velocityCommand.angular.z = erroAngle * 0.5
+        
+            self.velocity_publisher.publish(velocityCommand)
 
-        # Get the input from the user.
-        goal_pose.x = input("Set your x goal: ")
-        goal_pose.y = input("Set your y goal: ")
 
-        # Please, insert a number slightly greater than 0 (e.g. 0.01).
-        distance_tolerance = input("Set your tolerance: ")
 
-        vel_msg = Twist()
-
-        while self.euclidean_distance(goal_pose) >= distance_tolerance:
-
-            # Porportional controller.
-            # https://en.wikipedia.org/wiki/Proportional_control
-
-            # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(goal_pose)
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-
-            # Angular velocity in the z-axis.
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = self.angular_vel(goal_pose)
-
-            # Publishing our vel_msg
-            self.velocity_publisher.publish(vel_msg)
-
-            # Publish at the desired rate.
-            self.rate.sleep()
-
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
-
-        # If we press control + C, the node will stop.
-        rospy.spin()
-
-if __name__ == '__main__':
-    try:
-        x = TurtleBot()
-        x.move2goal()
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == "__main__":
+    warthog = Warthog("/odometry/filtered", "/cmd_vel", 0.05)
+    whileRate = rospy.Rate(10)
+    
+    while(not rospy.is_shutdown()):
+        warthog.move2goal()
+        whileRate.sleep()
